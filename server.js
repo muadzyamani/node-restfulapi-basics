cf71881126f312b3285a20258bb78c32dee398d5
Muadz <muadzyamani@gmail.com>
2023-10-12 14:16:05 +0800

    Start on database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const PORT = 3000;
@@ -7,6 +8,18 @@ app.get('/', (req, res) => {
     res.send('Hello from Node');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const runServer = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://root:PT9ukN1mO5bBQPgi@cluster0.kipkylu.mongodb.net/Node-Api?retryWrites=true&w=majority');
+        console.log('Connected to MongoDB!');
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}.`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+
+runServer();
